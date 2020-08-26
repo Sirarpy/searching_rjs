ff19f46d5cfc90b41c82b16457317331ddfa7dc3
Sirarpy <sipadavtyan@gmail.com>
2020-08-26 12:50:40 +0400

    Updates

---
+++ precache-manifest.b5aae48ce08e176bba5bc61e59f8b2c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1728e59b0ed6c53c3061be0a1055d9b",
+    "url": "/searching_rjs/index.html"
+  },
+  {
+    "revision": "62a502959dea5df54162",
+    "url": "/searching_rjs/static/css/2.2d5861c9.chunk.css"
+  },
+  {
+    "revision": "a7a43590cd6e9685333a",
+    "url": "/searching_rjs/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "62a502959dea5df54162",
+    "url": "/searching_rjs/static/js/2.24e711df.chunk.js"
+  },
+  {
+    "revision": "fa9f8d86bca7ba94baa2c0fa4755314d",
+    "url": "/searching_rjs/static/js/2.24e711df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a43590cd6e9685333a",
+    "url": "/searching_rjs/static/js/main.5f17f5c1.chunk.js"
+  },
+  {
+    "revision": "3ecc5f184e8d7a716d9d",
+    "url": "/searching_rjs/static/js/runtime-main.b28d65b8.js"
+  }
+]);
